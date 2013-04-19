15f789385dad42c07dff66383c44b9e1bce082ec
Sara Dadizadeh <sara@saradadizadeh.com>
2013-04-19 08:15:01 -0700

    add api

--- hello.py
+++ hello.py
@@ -3,10 +3,20 @@ from flask import Flask
 import datetime
 
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+
+@app.route('/')
+def index_page():
+    print 'index'
+
+@app.route('/create_user')
+def create_user():
+    print request.args.get('username')
+    print request.args.get('email')
 
 class BabarUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
