3c9a5e82a6612c91fcb461a3df71e3af59c6acbe
Sara Dadizadeh <sara@saradadizadeh.com>
2013-04-18 22:40:02 -0700

    wip

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask
+import datetime
+
+from pymongo import MongoClient
+client = MongoClient()
 
 app = Flask(__name__)
 
@@ -7,11 +11,31 @@ app = Flask(__name__)
 def hello():
     return 'index'
     
+@app.route('/show_users')
+def show_users():
+    connection = Connection()
+    db = connection.babardb
+    return db.users
 
 @app.route('/create_user/<username>')
 def create_user(username):
-    return 'User %s' % username
+    print username
+    # Get your DB
+    db = client.babardb
 
+    #_add_user(username)
+    return 'User %s added' % username
 
+def _add_user(username):
+    db = client.babardb
+    # Get your collection
+    users = db.users
+
+    # Create some objects
+    user = {"username": username,
+            "create_date": datetime.datetime.utcnow()}
+    # Insert it
+    users.insert(user)
+    return True
 
     
