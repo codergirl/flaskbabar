d3b2a630ab6ff784bae81741e449ac19d4d71619
Sara Dadizadeh <sara@saradadizadeh.com>
2013-04-18 21:59:52 -0700

    add api

--- hello.py
+++ hello.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'index'
+    
+
+@app.route('/create_user/<username/')
+def create_user(username):
+    return 'User %s' % username
+
+
+
     
