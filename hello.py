1fbe1e126e4c25d8483a27bb68681b6522958385
Sara Dadizadeh <sara@saradadizadeh.com>
2013-04-18 23:15:22 -0700

    db

--- hello.py
+++ hello.py
@@ -2,42 +2,12 @@ import os
 from flask import Flask
 import datetime
 
-from pymongo import MongoClient
-client = MongoClient()
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello():
-    return 'index'
-    
-@app.route('/show_users')
-def show_users():
-    connection = Connection()
-    db = connection.babardb
-    return db.users
 
-@app.route('/create_user/<username>')
-def create_user(username):
-    print username
-    # Get your DB
-    db = client.babardb
 
-    #_add_user(username)
-    return 'User %s added' % username
-
-def _add_user(username):
-    db = client.babardb
-    # Get your collection
-    users = db.users
-
-    # Create some objects
-    user = {"username": username,
-            "create_date": datetime.datetime.utcnow()}
-    # Insert it
-    users.insert(user)
-    return True
-
-    
